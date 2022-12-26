de02da8c4f0af264429fbd791d38b9e84470d3fd
yasinesenn <79712308+yasinesenn@users.noreply.github.com>
2022-12-27 02:10:22 +0300

    Node.java

--- DispatcherShell/src/Node.java
+++ DispatcherShell/src/Node.java
@@ -1,9 +1,11 @@
 public class Node<A> {
-	public Node<A> front;
-	public Node<A> back;
+	public Node<A> next;
+	public Node<A> prev;
 	public A data;
 
-	public Node(Node<A> front, Node<A> back, A data) {
-
+	public Node(A data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
 	}
 }
